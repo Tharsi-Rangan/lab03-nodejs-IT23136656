ec2f3e4ce6c0197d1581fbcc50ec2e63e10b9eee
Tharsi <tharsigaranganathan17@gmail.com>
2026-02-23 18:35:40 +0530

    Add read file example using fs.readFile

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Lab 03 NodeJS + GitHub Flow works!");
+
+const fs = require("fs");
+
+fs.readFile("file.txt", "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
